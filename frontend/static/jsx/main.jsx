72bcfe6a27ca746e955fb4bd38021bc3e1a040c6
Peeranut Chindanonda <PCXD@live.com>
2016-11-26 23:47:23 +0700

    Add update flat rate and tou

--- frontend/static/jsx/main.jsx
+++ frontend/static/jsx/main.jsx
@@ -48,20 +48,6 @@ var Users = React.createClass({
       </div>
     )
   },
-  broadcastPrice(e) {
-	e.preventDefault();
-	if(document.getElementById("new_price").value != "") {
-		socket.send(JSON.stringify([document.getElementById("date").value, document.getElementById("new_price").value]));
-		
-		var d = new Date(document.getElementById("date").value);
-		d.setHours(d.getHours() + 1);
-		
-		var newNum = (Math.random() * 3.3) + 0.7;
-		newNum = Math.round(newNum * 100) / 100;
-		document.getElementById("new_price").value = newNum;
-		document.getElementById("date").value = d.toISOString().slice(0, 19);
-	}
-  },
   componentDidMount() {
 	  var self = this;
 	  
@@ -139,15 +125,41 @@ var Users = React.createClass({
 	d.setSeconds(0);
 	document.getElementById('date').defaultValue = d.toISOString().slice(0, 19);
   },
+  broadcastPrice(e) {
+	e.preventDefault();
+	if(document.getElementById("new_price").value != "") {
+		socket.send(JSON.stringify({type: "realtime", date: document.getElementById("date").value, price: document.getElementById("new_price").value}));
+		
+		var d = new Date(document.getElementById("date").value);
+		d.setHours(d.getHours() + 1);
+		
+		var newNum = (Math.random() * 3.3) + 0.7;
+		newNum = Math.round(newNum * 100) / 100;
+		document.getElementById("new_price").value = newNum;
+		document.getElementById("date").value = d.toISOString().slice(0, 19);
+	}
+  },
+  updateFlatRate(event) {
+	  event.preventDefault()
+	  
+	  socket.send(JSON.stringify({type: "flatRate", r1: $("#r0").val(), r2:$("#r1").val(), r3:$("#r2").val()}));
+  },
+  updateTOU(event) {
+	  event.preventDefault()
+	  
+	  socket.send(JSON.stringify({type: "tou", peak: $("#peak").val(), offPeak:$("#offPeak").val(), monthly:$("#monthly").val()}));
+  },
   render() {
 	return (
 		<div className="container">
-			<h1>Broadcast price</h1>
+			<h1>Broadcast Realtime Price</h1>
+			<br/>
 			<form className="form-inline">
 				<div className="form-group">
 					<label htmlFor="date">Date</label>&nbsp;
 					<input className="form-control" type="datetime-local" id="date" name="date"/>
 				</div>
+				&nbsp;
 				<div className="form-group">
 					<label htmlFor="new_price">New price</label>&nbsp;
 					<input className="form-control" type="number" defaultValue="1.5" id="new_price" name="new_price" placeholder="New Price"/>
@@ -156,6 +168,65 @@ var Users = React.createClass({
 			</form>
 			<div id="container" style={{minWidth: "310px", height: "400px", margin: "0 auto"}}></div>
 			{this.state.prices.map(this.renderPrice)}
+			
+			<br/>
+			<h1>Update Flat Rate Price</h1>
+			<br/>
+			<form onSubmit={this.updateFlatRate}>
+			<table className="table table-bordered">
+				<thead>
+					<tr>
+						<th>Unit</th>
+						<th>Price</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope="row">0-150</th>
+						<td><input id="r0" type="number" required/></td>
+					</tr>
+					<tr>
+						<th scope="row">151-400</th>
+						<td><input id="r1" type="number" required/></td>
+					</tr>
+					<tr>
+						<th scope="row">401+</th>
+						<td><input id="r2" type="number" required/></td>
+					</tr>
+				</tbody>
+			</table>
+			<input className="btn btn-default" type="submit" name="submit" value="Update"/>
+			</form>
+			
+			<br/>
+			<h1>Update TOU Price</h1>
+			<br/>
+			<form onSubmit={this.updateTOU}>
+			<table className="table table-bordered">
+				<thead>
+					<tr>
+						<th colSpan="2">Price</th>
+						<th rowSpan="2">Monthly Fee</th>
+					</tr>
+					<tr>
+						<th>Peak</th>
+						<th>Off Peak</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><input id="peak" type="number" required/></td>
+						<td><input id="offPeak" type="number" required/></td>
+						<td><input id="monthly" type="number" required/></td>
+					</tr>
+				</tbody>
+			</table>
+			<input className="btn btn-default" type="submit" name="submit" value="Update"/>
+			</form>
+			
+			<br/>
+			<br/>
+			<br/>
 		</div>
 	)
     /*
